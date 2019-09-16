9af15970671cdc62e5190a514fa887729c329696
Luke Shumard <lukeshumard@gmail.com>
2019-09-16 11:48:46 +0100

    proper prompt setup

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,3 +1,3 @@
-ln -s $DOTFILES/zsh/.zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 mkdir -p -- "$HOME/.zim/modules/prompt/external-themes/kupppo/"
-ln -s $DOTFILES/zsh/kupppo.zsh-theme $HOME/.zim/modules/prompt/external-themes/kupppo/kupppo.zsh-theme
+ln -s $HOME/.dotfiles/zsh/kupppo.zsh-theme $HOME/.zim/modules/prompt/functions/prompt_kupppo_setup
