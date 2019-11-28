ce8131b4766009baae177b7ad770c33e8e190f7e
Luke Shumard <lukeshumard@gmail.com>
2019-11-28 03:07:15 +0000

    misc apps install + hack typeface

--- install.sh
+++ install.sh
@@ -25,20 +25,34 @@ xcode-select --install
 ./hyper.sh
 
 # Misc
+brew cask install postgres
+
 brew cask install iterm2
 
 brew cask install slack
+brew cask install telegram
+brew cask install signal
 
 brew cask install sketch
 brew cask install abstract
 
 brew cask install insomnia
 brew cask install graphiql
+brew cask install postman
 
 brew cask install google-chrome
 brew cask install firefox-developer-edition
 
 brew cask install dashlane
+
+brew cask install spotify
+# brew cask install sonos
+
+brew cask install tunnelblick
+
+# Hack typeface
+brew tap homebrew/cask-fonts
+brew cask install font-hack
 
 # SF Pro Mono
 ./sf-pro.sh
