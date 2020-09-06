58022af3af150b85c183622be653e50e781ef8e6
Luke Shumard <lukeshumard@gmail.com>
2020-09-07 00:29:40 +0100

    zsh updates

--- zsh.sh
+++ zsh.sh
@@ -1,14 +1,8 @@
+# Install ZSH
 brew install zsh
 
-git clone --recursive https://github.com/zimfw/zimfw.git ${ZDOTDIR:-${HOME}}/.zim
+# Install Zim
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 
-# for template_file in ${ZDOTDIR:-${HOME}}/.zim/templates/*; do
-#   user_file="${ZDOTDIR:-${HOME}}/.${template_file:t}"
-#   cat ${template_file} ${user_file}(.N) > ${user_file}.tmp && mv ${user_file}{.tmp,}
-# done
-
+# Symlink .zshrc
 ln -sfn $HOME/.dotfiles/config/zsh/.zshrc $HOME/.zshrc
-mkdir -p -- "$HOME/.zim/modules/prompt/external-themes/kupppo/"
-ln -sfn $HOME/.dotfiles/config/zsh/kupppo.zsh-theme $HOME/.zim/modules/prompt/functions/prompt_kupppo_setup
-
-chsh -s $(which zsh)
