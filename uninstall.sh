073949d810beb294f0860a55b0d7637897fc2c36
Luke Shumard <lukeshumard@gmail.com>
2020-11-03 12:04:17 +0000

    uninstall scripts

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,8 @@
 # Uninstall Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
 
-rm ~/.gitconfig_local
+rm $HOME/.gitconfig
+rm $HOME/.gitconfig_local
+rm $HOME/.zshrc
+rm $HOME/.hyper.js
+rm $HOME/.config/litecli/config
