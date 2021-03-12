54f2ddb896255a0a50db16f4954c02a727dd76c7
Luke Shumard <lukeshumard@gmail.com>
2021-03-12 23:27:42 +0000

    uninstall script: zim

--- uninstall.sh
+++ uninstall.sh
@@ -8,3 +8,10 @@ rm $HOME/.gitconfig_local
 rm $HOME/.gitignore
 rm $HOME/.zshrc
 rm $HOME/.hyper.js
+
+# Remove Zim
+rm $HOME/.zimrc
+rm -rf $HOME/.zim
+echo -e "Please check \033[1m~/.zshenv\033[0m and \033[1m~/.zlogin\033[0m"
+
+# TODO: Mac defaults
